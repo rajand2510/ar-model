2083a60b564c061a0c21eb0bba129fdf249a7637
Rajan Dhariyaparmar <rajandalvadi2510@gmail.com>
2024-06-29 12:42:02 +0530

    product

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import './App.css';
 import CubeConatainer from "./components/Cube/CubeConatainer";
 import XrCubeConatainer from './components/xr-cube/xrCubeConatainer';
 import XrHitModelConatainer from './components/xr-hit-model/XrHitModelConatainer';
-import ProductList from './components/xr-hit-model/ProductList';
+//import ProductList from './components/xr-hit-model/ProductList';
+import Products from './components/product/Products';
+
 
 function App() {
   return (
 <Routes>
-<Route path="/" element={<ProductList />} />
+<Route path="/" element={<Products />} />
 <Route path="/cube" element={<CubeConatainer />}/>
 <Route path="/xr-cube" element={<XrCubeConatainer />}/>
-{/* <Route path="/xr-hit-cube" element={<XrHitCubeConatainer />}/> */}
+<Route path="/xr-hit-cube" element={<XrCubeConatainer />}/>
 <Route path="/xr" element={<XrHitModelConatainer />}/>
 </Routes>
 
